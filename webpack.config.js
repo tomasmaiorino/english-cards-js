889902a9526091ae34cef958fa8fed909995bc7c
Tomas Maiorino <tomas.maiorino@compasso.com.br>
2018-04-26 16:40:34 -0300

    configuration for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,65 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+module.exports = (env, argv) => {
+  const mode = argv.mode;
+  console.log('environment ' + mode);
+  const config = {
+    mode: mode,
+    entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'index_bundle.js',
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader?url=false']
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader?url=false']
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          loaders: [{
+            loader: 'file-loader',
+            options: {
+              name(file) {
+                if (env === 'development') {
+                  return 'hash=sha512&digest=hex&name=[hash].[ext]'
+                }
+                return '[hash].[ext]'
+              }
+            }
+          },
             'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './assets/index.html'
-    })
-  ],
-  devServer: {
-    inline:true,
-    port: 3000,
-    host: "0.0.0.0"
+          ]
+        }
+      ]
+    },
+    plugins:
+      mode === 'production' ? [
+        new HtmlWebpackPlugin({
+          template: './assets/index.html'
+        }),
+        new CopyWebpackPlugin([
+          { from: '/assets/img/*', to: '/dist/assets/img/', context: '/' }
+        ], { debug: 'debug' })] : [new HtmlWebpackPlugin({
+          template: './assets/index.html'
+        })],
+    devServer: {
+      inline: true,
+      port: 3000,
+      host: "0.0.0.0"
+    }
   }
+  console.log("plugins length " + config.plugins.length);
+  return config;
 };
