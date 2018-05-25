53d81eca3b38af13b488432598c63caf8e26e7ba
tomasmaiorino <tomasmaiorino@gmail.com>
2018-05-24 22:02:38 -0300

    fix merge errors

--- src/lib/contact/Contact.js
+++ src/lib/contact/Contact.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Select from './../../Select';
 
-<<<<<<< HEAD
 // const CARD_URL = "http://localhost:8080/api/v1/content-types?q=grammar";
 // const CARD_URL_PROD = "https://fathomless-tundra-22713.herokuapp.com/api/v1/content-types?q=grammar";
 const token = 'qwetyuasdtyuer4rr';
@@ -9,8 +8,6 @@ const token = 'qwetyuasdtyuer4rr';
 const API = "http://localhost:40585/api/v1/messages/" + token;
 
 
-=======
->>>>>>> c5b3eb3a718e703d2e3d1e4588a5549a5ee0c48a
 class Contact extends Component {
 
   constructor(props) {
@@ -100,17 +97,9 @@ class Contact extends Component {
   }
 
   render() {
-<<<<<<< HEAD
 
     const enable = this.enableSubmit();
 
-=======
-     const {email, name, message, subject} = this.state;
-    // const isEmailValid = email === null || email.length > 10 && email.indexOf('@') !== -1;
-    // const isNameValid = name === null || name.length > 9 && /[a-zA-Z]+? [a-zA-Z]/.test(name);
-    // const isMessageValid = message === null || message.length > 10;
-    const enable = true;
->>>>>>> c5b3eb3a718e703d2e3d1e4588a5549a5ee0c48a
     return (
 
       <section id="contact">
@@ -119,7 +108,6 @@ class Contact extends Component {
           <div className="col-lg-5 col-md-12">
             <form className="p-5" id="contactForm">
               <div className="md-form form-sm">
-<<<<<<< HEAD
                 <i className="fa fa-user prefix grey-text" />
                 <input
                   type="text"
@@ -156,21 +144,6 @@ class Contact extends Component {
                   className="form-control"
                   id="senderEmail"
                 />
-=======
-                <i className="fa fa-user prefix grey-text"></i>
-                <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange} className="form-control" id="senderName" />
-                <label htmlFor="senderName">Your name *</label>
-              </div>
-              <div className="has-warning md-form form-sm">
-                <i className="fa fa-envelope prefix grey-text"></i>
-                <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange} className="form-control" id="senderEmail" />
-                <label htmlFor="senderEmail">Your email *</label>
-              </div>
-              <div className="has-error md-form form-sm">
-                <i className="fa fa-tag prefix grey-text"></i>
-                <input type="text" name="subject"
-                  className="form-control" value={this.state.subject} onChange={this.handleInputChange} id="subject" data-validation-required-message="Please enter a subject." />
->>>>>>> c5b3eb3a718e703d2e3d1e4588a5549a5ee0c48a
                 <label htmlFor="subject">Subject *</label>
                 <small className="form-text text-muted red-text" />
               </div>
@@ -183,7 +156,6 @@ class Contact extends Component {
                   style={{
                     height: 100
                   }}
-<<<<<<< HEAD
                   id="message"
                   onChange={this.handleMessage}
                   value={this.state.message}
@@ -199,14 +171,6 @@ class Contact extends Component {
                 >
                   Send <i className="fa fa-paper-plane-o ml-1" />
                 </button>
-=======
-                  id="message" data-validation-required-message="Please enter a message." value={this.state.message} onChange={this.handleInputChange}>
-                </textarea>
-                <label htmlFor="message">Your message *</label>
-              </div>
-              <div className="text-center">
-                <button className="btn btn-unique" disable={!enable}>Send <i className="fa fa-paper-plane-o ml-1"></i></button>
->>>>>>> c5b3eb3a718e703d2e3d1e4588a5549a5ee0c48a
               </div>
             </form>
           </div>
